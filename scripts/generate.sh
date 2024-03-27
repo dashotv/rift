ebf54f2a52c8b578ba286eb8d162dbb4df39e62b
Shawn Catanzarite <me@shawncatz.com>
2024-03-27 20:44:44 +0000

    add bingo for deps, still didn't fix issue with panic on 'gen' task

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,25 +2,32 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 BASE_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
 
+if [[ ! -e "$BASE_DIR/.bingo/variables.env" ]]; then
+  echo "Plese run 'task deps' first"
+  exit 1
+fi
+
+source "$BASE_DIR/.bingo/variables.env"
+
 SERVER="$BASE_DIR/internal/server"
 TEMPLATES="$BASE_DIR/internal/templates"
 DEFINITIONS="$BASE_DIR/internal/definitions"
 
 # Generate the code
 mkdir -p "$SERVER"
-oto -template "$TEMPLATES/echo.go.plush" \
+$OTO -template "$TEMPLATES/echo.go.plush" \
   -out "$SERVER/server.gen.go" \
   -ignore Ignorer \
   -pkg server \
   "$DEFINITIONS"
-oto -template "$TEMPLATES/models.go.plush" \
+$OTO -template "$TEMPLATES/models.go.plush" \
   -out "$SERVER/models.gen.go" \
   -ignore Ignorer \
   -pkg server \
   "$DEFINITIONS"
 
 mkdir -p "$BASE_DIR/client"
-oto -template "$TEMPLATES/client.go.plush" \
+$OTO -template "$TEMPLATES/client.go.plush" \
   -out "$BASE_DIR/client/client.gen.go" \
   -ignore Ignorer \
   -pkg client \
