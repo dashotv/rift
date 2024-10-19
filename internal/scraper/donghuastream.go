e6249587a40e51638f66801c977990caf1be4da3
Shawn Catanzarite <me@shawncatz.com>
2024-10-19 06:14:11 +0000

    fix: donghuastream scraping wasn't returning anything

--- internal/scraper/donghuastream.go
+++ internal/scraper/donghuastream.go
@@ -1,11 +1,12 @@
 package scraper
 
 import (
-	"fmt"
+	"encoding/base64"
 	"regexp"
 	"strconv"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly/v2"
 	"go.uber.org/zap"
 )
@@ -43,7 +44,37 @@ func (m *DonghuaStream) Read(url string) []string {
 func (m *DonghuaStream) ReadPage(url string) []string {
 	urls := []string{}
 	m.col.OnHTML("article", func(e *colly.HTMLElement) {
-		urls = append(urls, e.ChildAttr("iframe", "src"))
+		s := e.ChildAttr("iframe", "src")
+		if s == "" || s == "about:blank" {
+			return
+		}
+
+		urls = append(urls, s)
+	})
+	m.col.OnHTML("option", func(e *colly.HTMLElement) {
+		s := e.Attr("value")
+		if s == "" {
+			return
+		}
+
+		data, err := base64.StdEncoding.DecodeString(s)
+		if err != nil {
+			m.log.Errorf("base64: %s", err)
+			return
+		}
+
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(data)))
+		if err != nil {
+			m.log.Errorf("goquery: %s", err)
+			return
+		}
+
+		doc.Find("iframe").Each(func(i int, s *goquery.Selection) {
+			u, ok := s.Attr("src")
+			if ok {
+				urls = append(urls, u)
+			}
+		})
 	})
 	m.col.OnError(func(r *colly.Response, err error) {
 		m.log.Errorf("scraping: %s", err)
@@ -62,7 +93,8 @@ func (m *DonghuaStream) Parse(url string) []*Result {
 			if len(videos) == 0 {
 				continue
 			}
-			fmt.Printf("match: %+v\n", match[0])
+			// fmt.Printf("video: %+v\n", videos)
+			// fmt.Printf("match: %+v\n", match[0])
 			title := strings.Replace(match[0][1], "-", " ", -1)
 			season, _ := strconv.Atoi(match[0][2])
 			episode, _ := strconv.Atoi(match[0][3])
