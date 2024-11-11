0d40b1556e58918c539be14c7f12c7cf1ce5897c
Shawn Catanzarite <me@shawncatz.com>
2024-11-11 02:42:00 +0000

    animexin bug, messed up capture groups

--- internal/ytdlp/ytdlp_test.go
+++ internal/ytdlp/ytdlp_test.go
@@ -8,12 +8,13 @@ import (
 )
 
 func TestYtdlpList(t *testing.T) {
-	list, err := ProcessURL("https://jhdanime.live/2024/05/17/one-of-the-hidden-sect-leaders-is-shocking-ep-20/")
+	list, err := ProcessURL("https://myanime.live/2024/10/24/shixiong-a-shixiong-big-brother-episode-60-english-sub/")
 	require.NoError(t, err)
 	require.NotNil(t, list)
 	require.NotEmpty(t, list)
 
 	for _, e := range list {
+		fmt.Printf("name: %s\n", e.Fulltitle)
 		fmt.Printf("web: %s\n", e.WebpageURL)
 		fmt.Printf("url: %s\n", e.URL)
 		// spew.Dump(e)
